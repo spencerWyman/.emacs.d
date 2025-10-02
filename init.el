151e0c7b95fa1ed62877634c11526a3cde47175a
Spencer Wyman <spencer.wyman7@gmail.com>
2025-10-01 23:15:03 -0700

    Added auxiliary packages for lsp (ui and company), and pyright.

--- init.el
+++ init.el
@@ -124,6 +124,24 @@
   :config
   (lsp-enable-which-key-integration t))
 
+;; ;; we only use company for lsp pop-ups, otherwise use vertico
+(use-package company
+  :after lsp-mode ;; when not in lsp-mode, use vertico
+  :hook (lsp-mode . company-mode)
+  :custom
+  (company-minimum-prefix-length 1)
+  (company-idle-delay 0.0))
+
+(use-package lsp-ui
+  :hook (lsp-mode . lsp-ui-mode))
+
+;; ;; python 
+(use-package lsp-pyright
+  :custom (lsp-pyright-langserver-command "pyright")
+  :hook (python-mode . (lambda ()
+                          (require 'lsp-pyright)
+                          (lsp-deferred))))
+
 
 ;; auto-config
 
@@ -136,8 +154,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(corfu doom-modeline doom-themes lsp-mode magit marginalia orderless
-	   rainbow-delimiters vertico)))
+   '(company corfu doom-modeline doom-themes lsp-mode lsp-pyright lsp-ui
+	     magit marginalia orderless rainbow-delimiters vertico)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
